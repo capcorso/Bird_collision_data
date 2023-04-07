01fe6414ee6ad65dd0c3c40e9d3cc914c1f0848a
Julianna Corso <corsojk@g.cofc.edu>
2023-04-06 23:52:40 -0400

    Progress 4/6

--- Project work.R
+++ Project work.R
@@ -94,33 +94,61 @@ ggplot(top_sp_df, aes(x = reorder(how_obtained_code, -frequency), y = percentage
   geom_bar(stat = "identity", fill= mycolors) +
   xlab(" ") +
   ylab("%") +
-  ggtitle("Top 10 Species with most collisions") 
+  ggtitle("Top 10 Species with most collisions 1992-2017 ") 
 
 
 # Next renaming to be common name instead of species code
+# RTHA Red Tailed Hawk, BDOW Barred Owl, EASO Eastern Screech Owl, GHOW Great Horned Owl,OSPR  Osprey 
+# RSHA Red-Shouldered Hawk, COHA Cooper's Hawk, MIKI Mississippi Kite, TUVU Turkey Vulture, BAEA Bald Eagle 
+
+cleaned.species <- with(data, factor(Species_Abv, 
+                                     levels = c('RTHA', 'BDOW', 'EASO', 'GHOW', 'OSPR', 'RSHA', 'COHA', 'MIKI', 'TUVU', 'BEA'), 
+                                     labels = c("Red Tailed Hawk", "Barred Owl", "Eastern Screech Owl", "Great Horned Owl", "Osprey", "Red-Shouldered Hawk ", "Cooper's Hawk",
+                                                "Mississippi Kite", "Turkey Vulture", "Bald Eagle")))
 
 
-# RTHA red tailed Hawk 
+re.count <- table(cleaned.species)
 
-# BDOW Barred Owl 
+re_percent <- round(prop.table(re.count) * 100, 2)
 
-# EASO Eastern Screech Owl 
+re.top_sp <- names(sort(re.count, decreasing = TRUE)[1:10])
 
-# GHOW Great Horned Owl 
+re.data_sub <- data[cleaned.species %in% re.top_sp,]
 
-# OSPR  Osprey 
+re.sp_df <- as.data.frame(re.count)
 
-# RSHA Res-Shouldered Hawk 
+colnames(re.sp_df) <- c("how_obtained_code", "frequency")
 
-# COHA Cooper's Hawk
+re.sp_df$percentage <- re_percent
 
-# MIKI Mississippi Kite 
+re.top_sp_df <- re.sp_df[re.sp_df$how_obtained_code %in% re.top_sp,]
 
-# TUVU Turkey Vulture 
+ggplot(re.top_sp_df, aes(x = reorder(how_obtained_code, -frequency), y = percentage)) +
+  geom_bar(stat = "identity", fill= mycolors) +
+  xlab(" ") +
+  ylab("%") +
+  ggtitle("Top 10 Species with most collisions 1992-2017 ") 
 
-# BAEA Bald Eagle 
+#Yurrrhhhhh 
 
 
+
+
+cleaned.outcome <- with(data, factor(Disposition, 
+                                        levels = c('RELEASED', 'NR/EUTHANIZED', 'TRANSFERRED', 'EUTHANIZED', 'NR/DIED', 'DIED', 'TRANSFERRED OUT'), 
+                                        labels = c("Survived", "Dead", "Transferred", "Dead", "Dead", "Dead", "Transferred")))
+cleaned.outcome
+Dispo <- table(data$Disposition)
+list(data$Disposition)
+
+outcome.list <-list(cleaned.outcome)
+table(outcome.list)
+
+barplot(table(outcome.list))
+
+
+
+-------------------------------------------------------------------------------------------------------------
 
 #' Recieved new updated collision data from the ACC
 # Read in data 
@@ -140,4 +168,57 @@ list(new.sp.list)
 ct.new.sp <- table(new.sp.list)
 sort(ct.new.sp)
 
+# look at outcome 
+
+outcome <- new.data$Status
+list(outcome)
+outcome.ct <- table(outcome)
+sort(outcome.ct)
+
+# Grouping euthanized and dead with - dead. And Released with -Survive, Still in rehab is NA
+with(new.data, factor(Status, 
+                      levels = c('DOA', 'R', 'D', 'EOA', 'D24', 'E24', 'E'), 
+                      labels = c("Dead", "Survive", "Dead", "Dead", "Dead", "Dead", "Dead")))
+
+cleaned.status <- with(new.data, factor(Status, 
+                      levels = c('DOA', 'R', 'D', 'EOA', 'D24', 'E24', 'E'), 
+                      labels = c("Dead", "Survive", "Dead", "Dead", "Dead", "Dead", "Dead")))
+
+cleaned.status
+Survivorship <- table(cleaned.status)
+barplot.default(Survivorship)
+
+# Same species colsion bar graph for new data 
+
+new.sp_count <- table(new.data$Species)
+
+new.sp_percent <- round(prop.table(new.sp_count) * 100, 2)
+
+new.top_sp <- names(sort(new.sp_count, decreasing = TRUE)[1:10])
+
+new.data_sub <- new.data[new.data$Species %in% new.top_sp,]
+
+new.sp_df <- as.data.frame(new.sp_count)
+
+colnames(new.sp_df) <- c("how_obtained_code", "frequency")
+
+new.sp_df$percentage <- new.sp_percent
+
+new.top_sp_df <- new.sp_df[new.sp_df$how_obtained_code %in% new.top_sp,]
+
+# PLOTTTTT
+ggplot(new.top_sp_df, aes(x = reorder(how_obtained_code, -frequency), y = percentage)) +
+  geom_bar(stat = "identity") +
+  xlab(" ") +
+  ylab("%") +
+  ggtitle("Top 10 Species with most collisions 2019-2023") 
+
+# Plot with colors 
+mycolors.2 <- colorRampPalette(brewer.pal(8, "Pastel2"))(nb.cols)
+mycolors
+ggplot(top_sp_df, aes(x = reorder(how_obtained_code, -frequency), y = percentage)) +
+  geom_bar(stat = "identity", fill= mycolors.2) +
+  xlab(" ") +
+  ylab("%") +
+  ggtitle("Top 10 Species with most collisions") 
 
